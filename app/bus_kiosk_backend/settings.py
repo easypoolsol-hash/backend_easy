f75bbe84c77acbb89f71edf47e1a55bb88ed6e64
lalit_easypool <easypoolsol@gmail.com>
2025-10-27 17:06:16 +0530

    Add Firebase domain to CORS and CSRF trusted origins for admin access

--- app/bus_kiosk_backend/settings.py
+++ app/bus_kiosk_backend/settings.py
@@ -315,6 +315,7 @@ SECURE_HSTS_PRELOAD = True
 CORS_ALLOWED_ORIGINS = [
     "https://easypool.in",
     "https://www.easypool.in",
+    "https://easypool-30af3.web.app",  # Firebase hosting domain
     # REMOVED: HTTP localhost URLs should not be in production
     # "http://easypool.in",
     # "http://www.easypool.in",
@@ -327,6 +328,7 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "https://easypool.in",
     "https://www.easypool.in",
+    "https://easypool-30af3.web.app",  # Firebase hosting domain
 ]
 
 # DRF Spectacular settings
